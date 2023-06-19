142c8ee00d4e9c21f5c04d44b6503df423fbd8ce
vikash-A0 <thisisvikash75@gmail.com>
2023-06-19 23:30:49 +0530

    react-task

--- registrationtask/src/Home.js
+++ registrationtask/src/Home.js
@@ -3,7 +3,8 @@ import React from "react";
 const Home = () => {
   return (
     <div className="Home">
-      <h1>Amadis technologies</h1>
+      <h1>Welcome !!!</h1>
+      <h2>Registration Form</h2>
     </div>
   );
 };
